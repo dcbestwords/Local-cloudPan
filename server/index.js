77607a8d47f158d78e118f7f52613f76f7edc823
“dcbestwords” <1114686398@qq.com>
2026-07-03 20:33:25 +0800

    feat: mount auth routes + WebSocket server

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
+const http = require('http');
 const Koa = require('koa');
 const logger = require('koa-logger');
-const cors = require('@koa/cors'); // 跨域
-const { bodyParser } = require('@koa/bodyparser'); // 请求正文解析
-const static = require('koa-static'); // 静态文件服务
-const etag = require('koa-etag'); // etag缓存
-const compress = require('koa-compress'); // gzip压缩
+const cors = require('@koa/cors');
+const { bodyParser } = require('@koa/bodyparser');
+const static = require('koa-static');
+const etag = require('koa-etag');
+const compress = require('koa-compress');
 const config = require('./config');
+const { router: authRouter } = require('./controller/auth');
+const setupWebSocket = require('./controller/chat');
 
 const app = new Koa();
 app.use(logger());
 app.use(cors({ maxAge: 7200 }));
 app.use(bodyParser({ jsonLimit: '50mb' }));
-// 协商缓存设置304
 app.use(async (ctx, next) => {
   await next();
   if (ctx.fresh) {
@@ -19,10 +21,12 @@ app.use(async (ctx, next) => {
     ctx.body = null;
   }
 });
-app.use(etag()); // etag缓存
+app.use(etag());
 app.use(compress({ threshold: 2048, br: false }));
 app.use(static(config.global.publicPath));
 app.use(require('./controller/file').routes());
+app.use(authRouter.routes());
+app.use(authRouter.allowedMethods());
 
 // 处理错误
 app.on('error', (err, ctx) => {
@@ -43,6 +47,8 @@ app.on('error', (err, ctx) => {
   }
 });
 
-app.listen(config.global.port, () => {
+const server = http.createServer(app.callback());
+setupWebSocket(server);
+server.listen(config.global.port, () => {
   console.log(`Server start on port ${config.global.port}`);
 });
