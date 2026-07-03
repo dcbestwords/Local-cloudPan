82b8274e89133c50b65fb8a44a2ab686fba6ef19
“dcbestwords” <1114686398@qq.com>
2026-07-03 20:35:29 +0800

    feat: add chat types + Dexie composable

---
+++ admin/src/composables/useChatDB.ts
@@ -0,0 +1,24 @@
+import Dexie, { type Table } from 'dexie';
+
+class ChatDB extends Dexie {
+  messages!: Table<Message, number>;
+
+  constructor() {
+    super('chatDB');
+    this.version(1).stores({
+      messages: '++id, contact, time',
+    });
+  }
+}
+
+const db = new ChatDB();
+
+export function useChatDB() {
+  const getMessages = (contact: string): Promise<Message[]> =>
+    db.messages.where('contact').equals(contact).sortBy('id');
+
+  const saveMessage = (msg: Message): Promise<number> =>
+    db.messages.add(msg);
+
+  return { getMessages, saveMessage };
+}
---
+++ admin/src/typings/chat.d.ts
@@ -0,0 +1,12 @@
+interface Contact {
+  username: string;
+  online: boolean;
+}
+
+interface Message {
+  id?: number;
+  contact: string;
+  content: string;
+  time: string;
+  isSelf: boolean;
+}
